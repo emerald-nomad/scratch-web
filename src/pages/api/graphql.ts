19690f9f0a74c1bd3ea9473c47ca1863e3b6a535
Jalen Greene <33211911+emerald-nomad@users.noreply.github.com>
2021-07-07 15:13:54 -0500

    graphql endpoint

---
+++ src/pages/api/graphql.ts
@@ -0,0 +1,26 @@
+import { makeSchema, queryType } from 'nexus'
+import { ApolloServer } from 'apollo-server-micro'
+
+const Query = queryType({
+  definition(t) {
+    t.string('hello', { resolve: () => 'hello world!' })
+  },
+});
+
+const schema = makeSchema({
+  types: [Query],
+});
+
+const server = new ApolloServer({
+  schema,
+})
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+}
+
+export default server.createHandler({
+  path: '/api/graphql',
+})
