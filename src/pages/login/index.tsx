7d1831d2d1119c674a3c0351e7b63b0f78817760
Jalen Greene <33211911+emerald-nomad@users.noreply.github.com>
2021-07-08 14:58:55 +0000

    mobile login banner

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,5 +1,22 @@
+import Image from "next/image";
+import logo from "../../assets/images/logo.svg"
+import authBanner from "../../assets/images/auth-banner.jpeg";
+import styles from "./Login.module.scss"
+
 const Login: React.FC = () => {
-  return <h1>Login</h1>;
+  return (
+    <>
+      <div className={styles.banner}>
+        <Image src={logo} alt="Logo" />
+        <h1 className={styles['banner__header']}>Welcome Back!</h1>
+        <Image src={authBanner} alt="" />
+      </div> 
+
+      <section className={styles['banner__content']}>
+        <span className={styles['banner__message']}>Please login to continue.</span>
+      </section>
+    </>
+  );
 };
 
 export default Login;
