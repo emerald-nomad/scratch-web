b54a4fa60d87effa365b6c55153387221beec081
Jalen Greene <33211911+emerald-nomad@users.noreply.github.com>
2021-07-21 19:51:27 +0000

    adding apollo client

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,21 @@
 import "../styles/globals.scss";
 import Head from "next/head";
 import type { AppProps } from "next/app";
+import { useApollo } from "lib";
+import { ApolloProvider } from "@apollo/client";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const apolloClient = useApollo(pageProps);
+
   return (
-    <>
+    <ApolloProvider client={apolloClient}>
       <Head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Scratch</title>
       </Head>
       <Component {...pageProps} />
-    </>
+    </ApolloProvider>
   );
 }
 export default MyApp;
